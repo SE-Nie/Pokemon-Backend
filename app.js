d64982102208d59567c3b48752720b004a161215
Elton Benjamin <eltobenjo@gmail.com>
2020-12-02 13:30:12 +0100

    add cors

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require ('express');
 const app = express();
 const mongoose = require ('mongoose');
-require ('dotenv/config');
-let jsonData = require('./pokemondata.json')
+//require ('dotenv/config');
+let jsonData = require('./pokemondata.json');
+const cors = require ('cors');
+
+
 
 //Routes
 const pokemonRoute = require('./routes/pokemon');
 
+app.use(cors());
 app.use('/pokemon',pokemonRoute) ;
 
 
