01ebad0f0a45ca7ee7663560504bfc3fb41f2d3f
Svenja Niehus <svenja.niehus@gmail.com>
2020-12-02 17:10:14 +0100

    finished pokemon routing

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-const express = require ('express');
-const app = express();
-const cors = require('cors');
-const mongoose = require ('mongoose');
-require ('dotenv/config');
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const pokemonRoute = require('./routes/pokemon')
+//require ('dotenv/config');
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.json())
 app.use(cors())
+app.use(morgan('common'))
 //Routes
-const pokemonRoute = require('./routes/pokemon');
 
-app.use('/', pokemonRoute) ;
-
+app.use('/pokemon', pokemonRoute)
 
 //connect to DB
 //mongoose.connect(process.env.DB_CONNECTION,
-  //  {useNewUrlParser: true},
-    //()=> console.log('connected to DB!'))
+//  {useNewUrlParser: true},
+//()=> console.log('connected to DB!'))
 
-
-app.listen(4000,function (){
-console.log('Server started on 4000');
-});
+app.listen(4000, function () {
+	console.log('Server started on 4000')
+})
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,31 +1,35 @@
-const express = require ('express');
-const router = express.Router();
-const fs = require('fs');
-const rawdata = fs.readFileSync("pokemondata.json")
+const express = require('express')
+const router = express.Router()
+const fs = require('fs')
+const path = require('path')
+const rawdata = fs.readFileSync('routes/pokemondata.json')
 const pokemonData = JSON.parse(rawdata)
 
-router.get('/',(req, res)=>{
+router.get('/', (req, res) => {
+	console.log('pokemonData')
+	res.json(pokemonData)
+})
 
-    console.log(pokemonData);
-    res.send(pokemonData.name);
+router.get('/:id', (req, res) => {
+	const { id } = req.params
+	const match = pokemonData.find((p) => Number(p.id) === Number(id))
+	if (!match) {
+		return res.status(404).send('Pokemon with this ID does not exist')
+	}
+	res.json(match)
+})
 
-});
+router.get('/:id/:info', (req, res) => {
+	const { id, info } = req.params
+	const acceptableFields = new Set(['name', 'base', 'type'])
+	const match = pokemonData.find((p) => Number(p.id) === Number(id))
+	if (!match) {
+		return res.status(404).send('Pokemon with this ID does not exist')
+	}
+	if (!acceptableFields.has(info)) {
+		return res.status(400).send("You can't request this info type")
+	}
+	res.json(match[info])
+})
 
-
-router.get('/:id',(req, res)=>{
-    res.send('indiv by id');
-});
-
-
-
-router.get('/:id/:info',(req, res)=>{
-    res.send('name type base');
-});
-
-
-
-
-
-
-
-module.exports = router;
+module.exports = router
